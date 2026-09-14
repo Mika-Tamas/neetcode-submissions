911e5ecd914f1cbeebb82f02d509733a81cf342f
fractal <64777812+Mika-Tamas@users.noreply.github.com>
2026-09-14 13:49:58 +0200

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        PreOrder(root);
+        return root;
+    }
+
+    void PreOrder(TreeNode* root) {
+        if (root == NULL) return;
+        
+
+        TreeNode* temp = root->right;
+        root->right = root->left;
+        root->left = temp;
+        PreOrder(root->left);
+        PreOrder(root->right);
+    }
+};
